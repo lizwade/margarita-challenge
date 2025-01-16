15c827f1548375e37df10ef5de3386206c7c27f9
Liz Wade <11866103+lizwade@users.noreply.github.com>
2025-01-16 16:33:06 +0000

    Set up project, create data, describe kata

---
+++ OLDmain.js
@@ -0,0 +1,53 @@
+/**
+ *
+ * I love a margarita, and for some years now I've been trying to drink one in as many parts of the world as I can.
+ * 
+ * Sometimes I get a bit tipsy and forget where I've been.
+ * 
+ * Your task is, for any given position on a world map (represented by latitude and longitude), to remind me when I was nearest to that spot with a margarita in my hand.
+ * 
+ * Write a function that returns the date, as a string in the format yyyy-mm-dd, of the nearest margarita 
+ * 
+ * 
+ * 
+ * 
+ * Some examples:
+ * 
+ * getDateOfNearestMargarita(STUFFHERE, STUFFHERE, logbook) should return 20170402, because that was the date I had a margarita in Jerusalem
+ *  
+ * 
+ *  
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+// export function getDateOfNearestMargarita(longitute, latitude, logbook) {
+//     // start shaking!
+// }
+
+
+// I love a margarita, and for some years now I've been trying to drink one in as many parts of the world as I can.
+// I want to identify the weak spots in my global margarita coverage, so I can plan my next holiday accordingly.
+// Your task is to calculate the "Margarita Proximity" for any given position on a world map (represented by latitude and longitude)
+// Write a function that returns the distance (units??) from the point provided to the nearest margarita recorded in my margarita logbook
+// export function calculateMargaritaProximity(longitute, latitude, logbook) {
+//     // get shaking!
+// }
+
+
+/*
+Jerusalem 
+GPS Latitude Ref - North
+GPS Latitude - 31 deg 46' 52.89"
+GPS Longitude Ref - East
+GPS Longitude - 35 deg 13' 9.77"
+
+Ukraine
+GPS Version ID - 2.3.0.0
+GPS Latitude Ref - North
+GPS Latitude - 50 deg 27' 5.26"
+GPS Longitude Ref - East
+GPS Longitude - 30 deg 30' 45.14"
---
+++ main.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * I love a margarita, and for some years now I've been keeping a log of the various specimens I've sampled in different cities.
+ *
+ * Your task is, for any given date, to find which bar I was drinking margaritas in on the closest date to the given date, ie the fewest days away.
+ * If there is a tie, give me the name of the bar that served me the tastier margarita of the two (the one I gave the highest rating to).
+ *
+ * The date will be provided in a string in the format YYYY-MM-DD. You don't need to worry about time of day or timezones, just number of days.
+ *
+ * You will need the information in margarita_log. Your function should return the name of the bar as a string.
+ */
+export function findBarOfMargaritaNearestInTime(dateString, margarita_log) {}
